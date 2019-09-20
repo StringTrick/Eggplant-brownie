91aed868bf407f8e017b2c6cf57df2e412231f97
Patrick Batista de Arruda <patrick.batista.a@hotmail.com>
2019-09-20 14:38:19 -0300

    Exemplo de Array e laço For

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -11,3 +11,9 @@ add(name: "Brownie", calories: 30.5)
 add(name: "Brownie", calories: 30.5)
 add(name: "Eggplant", calories: 50.5)
 
+let calories = [10,20,30,40]
+
+for i in 0...3 {
+    print("Calorias são: \(calories[i])")
+}
+
