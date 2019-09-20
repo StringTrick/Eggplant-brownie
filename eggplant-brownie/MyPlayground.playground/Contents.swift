1d58ebe8760dffd6190a528a37ddeff55f0dd00e
Patrick Batista de Arruda <patrick.batista.a@hotmail.com>
2019-09-20 12:19:24 -0300

    Explicando Função com e sem Parâmetros

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+func add(name:String, calories:Double) {
+    print("adding \(name) with \(calories)")
+}
+
+add(name: "Brownie", calories: 30.5)
+add(name: "Brownie", calories: 30.5)
+add(name: "Eggplant", calories: 50.5)
+
