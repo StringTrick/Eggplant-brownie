b87a9324b0e66e5d48b3ac792fc13679971c6f05
Patrick Batista de Arruda <patrick.batista.a@hotmail.com>
2019-09-23 11:48:53 -0300

    Organizando grupos e adicionando fullCalories

--- eggplant-brownie/models/Meal.swift
+++ eggplant-brownie/models/Meal.swift
@@ -1,14 +1,24 @@
 import Foundation
 
 class Meal {
-    var name:String
-    var happiness:Int
-    var items = Array<Item>[]
+    let name:String
+    let happiness:Int
+    var items = Array<Item>()
 
     init(name:String, happiness:Int) {
         self.name = name
         self.happiness = happiness
     }
+    
+    func allCalories() -> Double {
+        var total = 0.0
+        for Item in items {
+            total += Item.calories
+        }
+        return total
+        
+    }
+    
     
     
 }
