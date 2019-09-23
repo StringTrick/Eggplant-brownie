bb46afced2a00b746b7070a32aaaefe633d9ad82
Patrick Batista de Arruda <patrick.batista.a@hotmail.com>
2019-09-23 10:13:03 -0300

    Adicionando classe Meal

--- eggplant-brownie/models/Meal.swift
+++ eggplant-brownie/models/Meal.swift
@@ -1,4 +1,15 @@
 import Foundation
 
+class Meal {
+    var name:String
+    var happiness:Int
+    var items = Array<Item>[]
 
+    init(name:String, happiness:Int) {
+        self.name = name
+        self.happiness = happiness
+    }
+    
+    
+}
 
