482e64ee6019fd758c8ccb794d6ebe78fb95350a
Patrick Batista de Arruda <patrick.batista.a@hotmail.com>
2019-09-25 14:49:20 -0300

    Adicionando comentários

--- eggplant-brownie/viewcontrollers/MealsTableViewController.swift
+++ eggplant-brownie/viewcontrollers/MealsTableViewController.swift
@@ -1,25 +1,25 @@
 import UIKit
 
 class MealsTableViewController : UITableViewController {
-    
+    //Declaração de array Meals
     var meals = [Meal(name: "Eggplant Brownie", happiness: 5),
                  Meal(name: "Cuzcuz", happiness: 1),
                  Meal(name: "Bife a cavalo", happiness: 4)]
-    
+    //Função adicionar refeição a tabela
     func add (meal:Meal) {
         meals.append(meal)
         tableView.reloadData()
     }
-    
+    //Função prepare para seguir o viewcontroller
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let view = segue.destination as! ViewController
         view.mealsTable = self
     }
-    
+    //Declaração de linhas da tabela
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return meals.count
     }
-    
+    //Declaração de celulas por linha
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = indexPath.row
         let meal = meals[row]
